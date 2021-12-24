3f058cc6e5a635de76f3531cfe87372d0cbfb8c8
VictorKazankov <sadovnichi@mail.ru>
2021-12-24 14:05:44 +0200

    Test get cookies

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,4 +1,12 @@
+from json import JSONDecodeError
+
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+
+try:
+    parse_response_text = response.json()
+    print(parse_response_text)
+except JSONDecodeError:
+    print("Response is not JSON format")
